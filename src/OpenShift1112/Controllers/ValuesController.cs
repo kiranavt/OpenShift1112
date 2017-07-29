d2945a898cf39be8d2e9b2237c477525c7809615
Kiran Purohit <kiran.purohit@rezblr.local>
2017-07-29 12:43:52 +0530

    mytest

---
+++ src/OpenShift1112/Controllers/ValuesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OpenShift1112.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        [Route("Myproject")]
+        [HttpPost]
+        public string Post(string name)
+        {
+            string a = "  Bangalore,Karnataka";
+            return name + a;
+        }
+
+        [Route("Addtion")]
+        [HttpPost]
+        public int Post(int  a,int b)
+        {
+            int c = a + b;
+            return c;
+        }
+    }
+}
